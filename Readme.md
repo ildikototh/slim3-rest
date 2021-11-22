21d1db9d6c18b70d177fbf07c6d81303c8c59c01
Ildiko <tothildikoeva@gmail.com>
2021-11-22 00:45:33 +0000

    update sql

--- Readme.md
+++ Readme.md
@@ -1,5 +1,37 @@
-Lepesek > 
+Lepesek 
+*slim [https://www.slimframework.com/docs/v3/]
 composer require slim/slim:3.*
-src/public konzvtar letrehozas
-index.php file
-routing az autloaderhez
+src/public konyvtar letrehozas
+index.php file:
+
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
+
+elso route tesztelese:
+php -S localhost:8080
+
+routing az autoloaderhez
+db table letrehozas
+CREATE TABLE IF NOT EXISTS `library` ( `book_id` int(11) NOT NULL AUTO_INCREMENT, `book_name` varchar(100) NOT NULL, `book_isbn` varchar(100) NOT NULL, `book_category` varchar(100) NOT NULL, PRIMARY KEY (book_id) ) DEFAULT CHARSET=utf8;
+
+INSERT INTO `library` (`book_id`, `book_name`, `book_isbn`, `book_category`) VALUES (1, 'PHP', 'bk001', 'Server Side'), (3, 'javascript', 'bk002', 'Client Side'), (4, 'Python', 'bk003', 'Data Analysis');
+
+db csatlakozas hozzaadasa index.phpben
+Get route hozzaadasa
+Post route hozzaadasa
+delete route
+Put route
+patch route
+
--- src/public/index.php
+++ src/public/index.php
@@ -15,13 +15,6 @@ $config['db']['dbname'] = 'test';
 $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
-$container['logger'] = function($c) {
-    $logger = new \Monolog\Logger('my_logger');
-    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
-    $logger->pushHandler($file_handler);
-    return $logger;
-};
-
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
     $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
@@ -31,48 +24,50 @@ $container['db'] = function ($c) {
     return $pdo;
 };
 
-// az adatbazisban levo konyvek lekerdezese
-// get method hasznalataval
-$app->get('/books', function(Request $request, Response $response) {
-    $query = $this->db->prepare("select * from library order by book_id");
+//sql query: SELECT * FROM library Order By book_id
+//Az adatbazisban levo konyvek lekerdezese
+//get 
+$app->get('/api/books', function (Request $request, Response $response) {
+    $sql_query = "SELECT * FROM library Order By book_id";
+    $query = $this->db->prepare($sql_query);
     $query->execute();
-    while( $row = $query->fetch(PDO::FETCH_ASSOC) ) {
-        $books[] = $row; // appends each row to the array
-    }
-    return $response->withJson(['data' => $books ], 200); 
+    $books = $query->fetchAll();
+    return $response->withJson(['data'=>$books], 200);
 });
 
-// Adott konyv Id alapjan keresese
-$app->get('/book/[{id}]', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT * FROM library WHERE book_id=:book_id");
-    $sth->bindParam("book_id", $args['id']);
-    $sth->execute();
-    $book = $sth->fetchObject();
-    return $this->response->withJson(['data' => $book ? $book : [] ], 200);
+$app->get('/api/book/{id}', function (Request $request, Response $response, $args) {
+    $sql_query = "SELECT * FROM library where book_id = :book_id";
+    $query= $this->db->prepare($sql_query);
+    $query->bindParam("book_id", $args['id']);
+    $query->execute();
+    $books = $query->fetchAll();
+    //if (!$books) $message = "Nincs az adott idval konyv";
+    return $response->withJson(['data'=>$books], 200);
 });
 
 // Konyv keresese megadott parameterek alapjan
-$app->get('/book/search/[{query}]', function ($request, $response, $args) {
+$app->get('/api/book/search/[{query}]', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM library WHERE UPPER(book_name) LIKE :query ORDER BY book_name");
     $query = "%".$args['query']."%";
     $sth->bindParam("query", $query);
-    var_dump($sth);
     $sth->execute();
     $books = $sth->fetchAll();
-    return $this->response->withJson($books);
+    return $this->response->withJson($books, 200);
 });
-        
+
+ 
 // Uj konyv hozzadasa
-$app->post('/book', function ($request, $response) {
+$app->post('/api/book', function ($request, $response) {
     $input = $request->getParsedBody();
     if(!empty($input['name'] && !empty($input['isbn']))){
-        $sql = "INSERT INTO library (book_name, book_isbn) VALUES (:name, :isbn)";
+        $sql = "INSERT INTO library (book_name, book_isbn, book_category) VALUES (:name, :isbn, :category)";
         $sth = $this->db->prepare($sql);
         $sth->bindValue(':name',$input['name']);
         $sth->bindValue(':isbn', $input['isbn']);
+        $sth->bindValue(':category', $input['category']);
         $sth->execute();
         $input['id'] = $this->db->lastInsertId();
-        $rsp['message'] =  $input['name'] . "konyv bekerult az adatbazisba, id ".  $input['id']  ;
+        $rsp['message'] =  $input['name'] . " konyv bekerult az adatbazisba, id ".  $input['id']  ;
     } else {
         $rsp["error"] = false;
         $rsp['message'] = "Hianyzo adatok, konyv cime es isbn kell" ;
@@ -82,40 +77,70 @@ $app->post('/book', function ($request, $response) {
         ->withJson($rsp);
 });
      
-
-// konyv torlese id alapjan
+//konyv torlese id alapjan
 //methodus: DELETE
 //parameter: id 
-$app->delete('/book/[{id}]', function ($request, $response, $args) {
-    $sth = $this->db->prepare("DELETE FROM library WHERE book_id=:id");
-    $sth->bindParam("id", $args['id']);
-    $sth->execute();
-    return $response->withJson(['message' => 'Book with id='. $args['id']. ' deleted' ], 200); 
+$app->delete('/api/book/delete/[{id}]', function ($request, $response, $args) {
+    $query = "DELETE FROM library WHERE book_id=:id";
+    try {
+        $sth = $this->db->prepare($query);
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
+        $rsp["error"] = false;
+        $rsp["message"] = 'A ' .$args['id'] . ' idju konyv sikeresen torolve';
+    } catch (PDOException $e) {
+      // a konyvet nem sikerult torolni
+      $rsp["error"] = true;
+      $rsp["message"] = 'Hiba a '. $args['id']. ' idju konyv torlese soran!';
+    }
+
+    return $response
+        ->withStatus(200)
+        ->withJson($rsp); 
 });
     
 // ID alapjan konyv cimenek updatelese -> a teljes adatsor updatelodik
 //methodus: put
-$app->put('/book/[{id}]', function ($request, $response, $args) {
+$app->put('/api/book/[{id}]', function ($request, $response, $args) {
     $input = $request->getParsedBody();
-    $sql = "UPDATE library SET book_name=:name WHERE book_id=:id";
-    $sth = $this->db->prepare($sql);
-    $sth->bindParam("id", $args['id']);
-    $sth->bindParam("name", $input['name']);
-    $sth->execute();
-    $input['id'] = $args['id'];
-    return $this->response->withJson($input);
+    $sql = "UPDATE library SET book_name=:name, book_isbn=:isbn, book_category=:category WHERE book_id=:id";
+    try {
+        $sth = $this->db->prepare($sql);
+        $sth->bindParam("id", $args['id']);
+        $sth->bindValue(':name',$input['name']);
+        $sth->bindValue(':isbn',$input['isbn']);
+        $sth->bindValue(':category',$input['category']);
+        $sth->execute(); 
+        $rsp["error"] = false;
+        $rsp["message"] = 'A ' .$args['id'] . ' idju konyv sikeresen frissitve';
+    } catch (Exception $e) {
+        $rsp["error"] = true;
+        $rsp["message"] = $e.getMessage();
+    }
+    return $response
+        ->withStatus(200)
+        ->withJson($rsp); 
 });
 
-// Reszleges update
-$app->patch('/book/:id', function ($id) {
+// ID alapjan konyv cimenek updatelese -> reszleges frissites
+//methodus: patch
+$app->patch('/api/book/[{id}]', function ($request, $response, $args) {
     $input = $request->getParsedBody();
-    $sql = "UPDATE library SET book_category=:category WHERE book_id=:id";
-    $sth = $this->db->prepare($sql);
-    $sth->bindParam("id", $args['id']);
-    $sth->bindParam("category", $input['category']);
-    $sth->execute();
-    $input['id'] = $args['id'];
-    return $this->response->withJson($input);
+    $sql = "UPDATE library SET book_name=:name WHERE book_id=:id";
+    try {
+        $sth = $this->db->prepare($sql);
+        $sth->bindParam("id", $args['id']);
+        $sth->bindValue(':name',$input['name']);
+        $sth->execute(); 
+        $rsp["error"] = false;
+        $rsp["message"] = 'A ' .$args['id'] . ' idju konyv sikeresen frissitve';
+    } catch (Exception $e) {
+        $rsp["error"] = true;
+        $rsp["message"] = $e;
+    }
+    return $response
+        ->withStatus(200)
+        ->withJson($rsp); 
 });
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
